834aec1bdd4ea83c5cd8a42ba1f42b28a29226f0
liudengfeng <liudengfeng@taoche.com>
2023-06-15 11:27:17 +0800

    享元模式demo

--- flyweightPattern/main.py
+++ flyweightPattern/main.py
@@ -2,6 +2,10 @@ from flyweightPattern.flyweightFactory import FlyweightFactory
 from flyweightPattern.iFlyweight import IFlyweight
 from flyweightPattern.unsharedConcreteFlyweight import UnsharedConcreteFlyweight
 
+"""
+享元模式demo
+该模式建议将一个大对像拆解为若干组合的小对像,在设计时需要区分外部状态和内部状态,通过外部状态的共享从而实现其中部分属性的重用
+"""
 if __name__ == "__main__":
     factory = FlyweightFactory()
     obj: IFlyweight = factory.getFlyweight("one")
