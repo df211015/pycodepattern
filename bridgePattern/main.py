40b1680c3b056a634ecfc0829a0c35fac52f97fb
liudengfeng <liudengfeng@taoche.com>
2023-06-15 14:19:57 +0800

    桥接模式demo

--- bridgePattern/main.py
+++ bridgePattern/main.py
@@ -1,6 +1,10 @@
 from bridgePattern.concreteImpliment import ConcreteImpliment
 from bridgePattern.concreteService import ConcreteService
 
+"""
+桥接模式demo
+将实现和抽象分离开来,各自有自己的开发维护空间
+"""
 if __name__ == "__main__":
     impliment = ConcreteImpliment()
     service = ConcreteService(impliment)
