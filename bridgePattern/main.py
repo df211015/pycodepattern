e1795b2f121a64df593e95c36a0b5f39226419d9
liudengfeng <liudengfeng@taoche.com>
2023-06-15 15:44:08 +0800

    门面模式demo

--- bridgePattern/main.py
+++ bridgePattern/main.py
@@ -2,7 +2,7 @@ from bridgePattern.concreteImpliment import ConcreteImpliment
 from bridgePattern.concreteService import ConcreteService
 
 """
-桥接模式demo
+桥梁模式demo
 将实现和抽象分离开来,各自有自己的开发维护空间
 """
 if __name__ == "__main__":
